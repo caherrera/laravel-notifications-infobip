7bbda849b9b4594081877dab3c932b8ea4d97a8b
Carlos Herrera <carlos.herreracaceres@cencosud.cl>
2021-03-03 16:10:27 -0300

    removing constructor

--- src/InfobipMessage.php
+++ src/InfobipMessage.php
@@ -7,17 +7,5 @@ use infobip\api\model\omni\send\WhatsAppData;
 class InfobipMessage extends WhatsAppData
 {
 
-    /**
-     * Create a new message instance.
-     *
-     * @param  array  $content
-     */
-    public function __construct($content = [])
-    {
-        $this->setTemplateName($content['templateName'] ?? null);
-        $this->setTemplateNamespace($content['templateNamespace'] ?? null);
-        $this->setTemplateData($content['templateData'] ?? null);
-        $this->setLanguage($content['language'] ?? null);
-    }
 
 }
